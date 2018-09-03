db6c90248ad383388863585082ffba59d5f458a5
mostrozny <ostrozny.michal@gmail.com>
2018-09-03 11:32:15 +0200

    base

---
+++ src/Table.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Table extends Component {
+    render() {
+        return (
+            <div>
+                blabla
+            </div>
+        );
+    }
+}
+
+export default Table;
